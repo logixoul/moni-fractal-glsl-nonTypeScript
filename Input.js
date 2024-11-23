5de0c5b637d78e227e51499673755e93c5ac9250
Stefan Monov <logixoul@gmail.com>
2024-11-23 16:29:22 +0200

    zoom works awesomely now

--- Input.js
+++ Input.js
@@ -1,7 +1,11 @@
 import { globals } from "./Globals.js";
 import * as THREE from 'three';
 import * as KeysHeld from './KeysHeld';
-const getRelativePoint = (pixel, length, set) => set.start + (pixel / length) * (set.end - set.start);
+
+function mix(x, y, a) {
+    return x*(1-a)+y*a;
+}
+
 export class Input {
     constructor() {
         this.addSlider("numIterations", 1, 2000);
@@ -12,54 +16,29 @@ export class Input {
             //e.preventDefault();
             if (e.button != 0)
                 return;
-            const WIDTH = window.innerWidth;
-            const HEIGHT = window.innerHeight;
-            const ZOOM_FACTOR = e.deltaY < 0 ? 0.1 : 1.0;
-            const zfw = (WIDTH * ZOOM_FACTOR);
-            const zfh = (HEIGHT * ZOOM_FACTOR);
+            const ZOOM_FACTOR = e.deltaY < 0 ? 0.1 : -0.1;
             const m = unproject(e.x, e.y, globals.stateTex.get());
+            //const ratioX = m.x / (1.0 - m.x);
+            //const ratioY = m.y / (1.0 - m.y);
+            console.log("m=", m);
+
             globals.REAL_SET = {
-                start: getRelativePoint(m.x - zfw, WIDTH, globals.REAL_SET),
-                end: getRelativePoint(m.x + zfw, WIDTH, globals.REAL_SET)
+                start: mix(globals.REAL_SET.start, m.x, ZOOM_FACTOR),
+                end: mix(globals.REAL_SET.end, m.x, ZOOM_FACTOR)
             };
             globals.IMAGINARY_SET = {
-                start: getRelativePoint(m.y - zfh, HEIGHT, globals.IMAGINARY_SET),
-                end: getRelativePoint(m.y + zfh, HEIGHT, globals.IMAGINARY_SET)
+                start: mix(globals.IMAGINARY_SET.start, m.y, ZOOM_FACTOR),
+                end: mix(globals.IMAGINARY_SET.end, m.y, ZOOM_FACTOR)
             };
         });
         function unproject(x, y, tex) {
-            return new THREE.Vector2(x, tex.image.height - 1 - y);
+            let val = new THREE.Vector2(x, tex.image.height - 1 - y);
+            val.x /= window.innerWidth;
+            val.y /= window.innerHeight;
+            val.x = mix(globals.REAL_SET.start, globals.REAL_SET.end, val.x);
+            val.y = mix(globals.IMAGINARY_SET.start, globals.IMAGINARY_SET.end, val.y);
+            return val;
         }
-        function drawLine(p1Projected, p2Projected) {
-            const p1 = unproject(p1Projected.x, p1Projected.y, globals.stateTex.get());
-            const p2 = unproject(p2Projected.x, p2Projected.y, globals.stateTex.get());
-            for (var i = 0; i < 1; i += 0.1 / p1.distanceTo(p2)) {
-                const p = p1.lerp(p2, i);
-                drawCircleToTex(globals.stateTex, p);
-            }
-        }
-        document.addEventListener("mousemove", e => {
-            if ((e.buttons & 1) != 0 || (e.buttons & 2) != 0) {
-                //paintMaterial.color = e.buttons & 1 ? new THREE.Color(1,1,1) : new THREE.Color(0, 0, 0);
-                //drawLine(new THREE.Vector2(e.x, e.y), new THREE.Vector2(e.x - e.movementX, e.y - e.movementY));
-            }
-        });
-        var lastTouchPos;
-        document.addEventListener("touchstart", e => {
-            document.getElementById("intro").style.display = "none";
-        });
-        document.addEventListener("touchend", e => {
-            lastTouchPos = undefined;
-        });
-        document.addEventListener("touchmove", e => {
-            var t = e.changedTouches[0];
-            var pos = new THREE.Vector2(t.clientX, t.clientY);
-            if (lastTouchPos === undefined)
-                lastTouchPos = pos;
-            paintMaterial.color = new THREE.Color(1, 1, 1);
-            drawLine(pos, lastTouchPos);
-            lastTouchPos = pos;
-        });
         document.addEventListener("keydown", e => {
             const char = e.code.toLowerCase();
             KeysHeld.global_keysHeld[char] = true;
